4cc6b3d5d5e8812405abc914ac790a594bef0dc1
liuzhefeng <756792312@qq.com>
2021-03-23 20:00:17 +0800

    xuexi

--- 手写数字/number_test.py
+++ 手写数字/number_test.py
@@ -1,2 +1,17 @@
 import torch
-import torchvision.datasets as datasets
+import torchvision.datasets as dataset
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
+train_data = dataset.MNIST(root="mnist",
+                           train=True,
+                           transform=transforms.ToTensor(),
+                           download=True)
+
+test_data = dataset.MNIST(root="mnist",
+                           train=False,
+                           transform=transforms.ToTensor(),
+                           download=False)
+
+
+
