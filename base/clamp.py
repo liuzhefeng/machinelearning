7706379689d7ad784578e58e937252ab6e5cbcc5
liuzf <liuzf@digitalcitylab.com.cn>
2021-03-29 19:57:33 +0800

    afsdd

--- base/clamp.py
+++ base/clamp.py
@@ -3,6 +3,10 @@ import torch
 a = torch.rand(2, 2) * 10
 
 print(a)
+#
+#       | min, if x_i < min
+# y_i = | x_i, if min <= x_i <= max
+#       | max, if x_i > max
 a = a.clamp(2, 5)
 
 print(a)
--- base/demo_attribute.py
+++ base/demo_attribute.py
@@ -1,7 +1,7 @@
 import torch
 
 dev = torch.device("cpu")
-dev = torch.device("cuda")
+# dev = torch.device("cuda")
 a = torch.tensor([2, 2],
                  dtype=torch.float32,
                  device=dev)
@@ -14,8 +14,8 @@ a = torch.sparse_coo_tensor(i, v, (4, 4),
                             device=dev).to_dense()
 print(a)
 
-
-
-
-
-
+indices = torch.tensor([[4, 2, 1], [2, 0, 2]])
+values = torch.tensor([3, 4, 5], dtype=torch.float32)
+# x = torch.sparse_coo_tensor(indices=indices, values=values, size=[5, 5])
+x = torch.sparse_coo_tensor(indices, values, (5, 5), device=dev).to_dense()
+print(x)
